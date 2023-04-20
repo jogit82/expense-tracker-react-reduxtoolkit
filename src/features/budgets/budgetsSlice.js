657319e7cd2b259ed45189134b230674891c0bd5
Jo <jo@iMac.kitchenpc.net>
2023-04-20 15:42:52 -0700

    deleted old code and cleaned up exports

--- src/features/budgets/budgetsSlice.js
+++ src/features/budgets/budgetsSlice.js
@@ -19,36 +19,15 @@ const budgetsSlice = createSlice({
   name: "budgets",
   initialState: initialState,
   reducers: {
-    ditBudget: (state, action) => {
-      state.map((state) => ({
-        category: action.payload.category,
-        amount: action.payload.ammount,
-      }));
+    editBudget: (state, action) => {
+      state.map((budget) => {
+        if (budget.category === action.payload.category) {
+          budget.amount = action.payload.amount;
+        }
+      });
     },
   },
 });
 
-export const editBudget = (budget) => {
-  return {
-    type: "budgets/editBudget",
-    payload: budget,
-  };
-};
-
-const budgetsReducer = (state = initialState, action) => {
-  switch (action.type) {
-    case "budgets/editBudget":
-      const newBudgets = state.map((budget) => {
-        if (budget.category === action.payload.category) {
-          return action.payload;
-        }
-        return budget;
-      });
-      return newBudgets;
-    default:
-      return state;
-  }
-};
-
-export const selectBudgets = (state) => state.budgets;
-export default budgetsReducer;
+export const { editBudget } = budgetsSlice.actions;
+export default budgetsSlice.reducer;
